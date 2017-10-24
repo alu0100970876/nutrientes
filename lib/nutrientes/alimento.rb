cf3a3129e721e7f2cb4420b2fdf4d624e5312cf9
Miguel Jiménez Gomis <alu0100970876@ull.edu.es>
2017-10-24 11:40:38 +0100

    Failure: Se almacena correctamente la cantidad de lipidos en grmaos ARREGLADO

--- lib/nutrientes/alimento.rb
+++ lib/nutrientes/alimento.rb
@@ -1,10 +1,11 @@
 class Alimento
 	
-	attr_reader :nombre, :proteinas, :glucidos
+	attr_reader :nombre, :proteinas, :glucidos, :lipidos
 
 	def initialize(nombre, p, g, l)
 		@nombre = nombre
 		@proteinas = p
 		@glucidos = g
+		@lipidos = l
 	end
 end
