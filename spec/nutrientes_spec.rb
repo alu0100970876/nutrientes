0e5dca8d5ced96ebb3ac6a2ecd6598e72347cdcc
Miguel Jiménez Gomis <alu0100970876@ull.edu.es>
2017-10-24 18:00:44 +0100

    Failure: Metodo que retorna valor energético (FAILED - 1)

--- spec/nutrientes_spec.rb
+++ spec/nutrientes_spec.rb
@@ -15,7 +15,8 @@ RSpec.describe Nutrientes do
   	end 
 
 	describe "#Se almacenan los datos" do
-    		it "Se almacena correctamente el nombre" do
+    		
+		it "Se almacena correctamente el nombre" do
       			expect(@ejemplo.nombre).to eq("Ejemplo") 
     		end
 
@@ -33,8 +34,16 @@ RSpec.describe Nutrientes do
   	end
 
 	describe "#Metodos para obtener info formateada" do
+		
 		it "Metodo para obtener un alimento formateado" do
 			expect(@ejemplo.to_s).to eq("Nombre: Ejemplo, Proteinas: 10gr, Glúcidos: 5gr, Lipidos: 1gr")
+		end
+	end
+
+	describe "#Metodos para obtener el valor energético de un alimento" do
+		
+		it"Metodo que retorna valor energético" do
+			expect(@ejemplo.v_energetico).to eq(69)
 		end
 	end
 
